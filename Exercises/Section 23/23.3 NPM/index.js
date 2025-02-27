18faf1a2c6ca69648d0338ccb08dc915b2d2c900
Eric Colvin <ecolvin@mail.csuchico.edu>
2025-02-27 14:41:26 -0800

    Node.js Exercises

---
+++ Exercises/Section 23/23.3 NPM/index.js
@@ -0,0 +1,5 @@
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+
+console.log("Look! Up in the sky!");
+console.log(`It's a bird! It's a plane!! It's ${name}!!!`);
--- Exercises/Section 23/23.4 QR Code Project/index.js
+++ Exercises/Section 23/23.4 QR Code Project/index.js
@@ -3,3 +3,28 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+import {writeFile, createWriteStream} from "node:fs";
+import inquirer from "inquirer";
+import {image} from "qr-image";
+
+inquirer.prompt([
+    { 
+        message: "Please enter a URL to be turned into a QR code!",
+        name: "URL",
+    }
+]).then((answers) => {
+    var url = answers.URL;
+    var qrCode = image(url);
+    qrCode.pipe(createWriteStream('qr_code.png'));
+    writeFile("URL.txt", url, err => {
+        if(err)
+            throw err;
+        console.log(`User input \"${url}\" written to URL.txt`);
+    });
+}).catch((error) => {
+    if(error.isTtyError)
+        console.log("Prompt couldn't be rendered in the current environment.");
+    else
+        throw error;
+});
